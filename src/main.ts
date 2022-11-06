58aa9610312a005bc58ad27fbc30ff5dd5733b93
刘伟 <lemonpaimc@126.com>
2022-11-06 21:21:58 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { join } from 'path'
+import * as env from 'dotenv';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const filepath: string = join(process.cwd(), core.getInput('path'));
+    const filter: string[] = core.getInput('filter').split(',');
+    core.info(`环境文件地址：${filepath}`);
+    env.config({ path: filepath });
+    const code = Object.entries(process.env).filter((item) => filter.includes(item[0])).reduce((a: string[], b) => a.concat([`${b[0]}=${b[1]}`]), []).join(' ');
+    core.info(`变量：${code}`);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('env', code)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
