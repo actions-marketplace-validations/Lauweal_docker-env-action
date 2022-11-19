4157918ebe9f2dd9192822d0ecf39b6fa6258499
刘伟 <lemonpaimc@126.com>
2022-11-19 15:31:20 +0800

    feat(env): 环境转换为对象

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,14 @@
 import * as core from '@actions/core'
-import {join} from 'path'
+import { join } from 'path'
 import * as env from 'dotenv'
 
 async function run(): Promise<void> {
   try {
     const filepath: string = join(process.cwd(), core.getInput('path'))
-    const filter: string[] = core.getInput('filter').split(',')
     core.info(`环境文件地址：${filepath}`)
-    const config = env.config({path: filepath})?.parsed
-    const code = Object.entries(process.env)
-      .filter(item => filter.includes(item[0]))
-      .reduce((a: string[], b) => a.concat([`${b[0]}=${b[1]}`]), [])
-      .join('\n')
-    core.info(JSON.stringify(config))
+    const config = env.config({ path: filepath })?.parsed
 
-    core.setOutput('env', code)
+    core.setOutput('env', config)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
